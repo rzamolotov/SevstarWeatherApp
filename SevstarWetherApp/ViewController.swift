a2e66118955331098ff6ed6d604b83b055422e81
Roman Zamolotov <rzamolotov@gmail.com>
2023-04-21 16:45:33 +0300

    Initial Commit

---
+++ SevstarWetherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevstarWetherApp
+//
+//  Created by Роман Замолотов on 21.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
